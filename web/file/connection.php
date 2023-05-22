57342f88948a6a80295e5fa54b170d9348642712
Sviat Pinkevych <sviat330@gmail.com>
2023-05-22 05:12:47 +0300

    Change connection.php for using env vars

--- web/file/connection.php
+++ web/file/connection.php
@@ -1,8 +1,8 @@
 <?php
-$servername ="localhost";
-$username ="bloodbank";
-$password = "wasd74";
-$dbname = "bloodbank";
+$servername = getenv('DB_HOST');;
+$username = getenv('DB_USER');
+$password = getenv('DB_PASS');
+$dbname = getenv('DB_NAMES');
 $conn = new mysqli($servername, $username, $password, $dbname);
 if(!$conn){
  die('Could not Connect MySql:' .mysql_error());
