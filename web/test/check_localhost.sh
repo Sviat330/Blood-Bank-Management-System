3d97da7773a43dac2384066bd76a06cc71857e07
Sviat Pinkevych <sviat330@gmail.com>
2023-05-23 05:47:27 +0300

    TEstt

--- web/test/check_localhost.sh
+++ web/test/check_localhost.sh
@@ -4,6 +4,7 @@
 response=$(curl -s http://localhost:80)
 
 # Check if the word "blood" is present in the response
+echo $response
 if [[ $response != *"blood"* ]]; then
     echo "The word 'blood' is not found."
     exit 1
