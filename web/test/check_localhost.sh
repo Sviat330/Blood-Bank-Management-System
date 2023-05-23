c42334c6433549aab012434d3c76ef3db1a4d713
Sviat Pinkevych <sviat330@gmail.com>
2023-05-23 06:40:58 +0300

    Change web/test/check_localhost.sh

--- web/test/check_localhost.sh
+++ web/test/check_localhost.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Make a GET request to localhost and store the response in a variable
-id=${docker ps --filter "name=blood-bank-management-system-nginx-1" | awk '{print $1}' | head -2 | tail -1}
+id=$(docker ps --filter "name=blood-bank-management-system-nginx-1" | awk '{print $1}' | head -2 | tail -1)
 
-response=$(docker exec 58c66211ee0d curl localhost)
+response=$(docker exec ${id} curl localhost)
 
 # Check if the word "blood" is present in the response
 if [[ $response != *"blood"* ]]; then
