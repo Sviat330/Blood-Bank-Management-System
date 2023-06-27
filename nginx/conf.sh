5d28f729f747e478d15cfa2b1220aab5fad85789
Sviat Pinkevych <sviat330@gmail.com>
2023-06-27 18:39:23 +0300

    Change file nginx/conf.sh for correct ecs work

--- nginx/conf.sh
+++ nginx/conf.sh
@@ -4,7 +4,7 @@ export PHP_FPM_PORT="9000"
 # Retrieve IP address of the PHP-FPM container
 PHP_FPM_IP=$(curl  ${ECS_CONTAINER_METADATA_URI_V4}/task | jq -r '.Containers[-1].Networks[].IPv4Addresses[0]')
 OWN_IP=$(hostname -i)
-if [[ $OWN_IP == $PHP_FPM_IP ]]
+if [ "$OWN_IP" == "$PHP_FPM_IP" ]
 then
 PHP_FPM_IP=$(curl  ${ECS_CONTAINER_METADATA_URI_V4}/task | jq -r '.Containers[0].Networks[].IPv4Addresses[0]')
 fi
@@ -14,4 +14,3 @@ export PHP_FPM_HOST=$PHP_FPM_IP
 # Start Nginx
 envsubst < /root/bloodbank_docker >/etc/nginx/conf.d/default.conf
 nginx -g "daemon off;"
-
